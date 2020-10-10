dc4557c66cc0c96ed1f7d8cefb352a5222a20e5a
yuesong-feng <yuesong-feng@foxmail.com>
2020-10-10 11:02:57 +0100

    add components message

--- src/components/conv2d.vue
+++ src/components/conv2d.vue
@@ -1,16 +1,10 @@
 <template>
-  <v-card class="mx-auto" max-width="344" outlined>
+  <v-card class="mx-auto" max-width="600" outlined>
     <v-list-item three-line>
       <v-list-item-content>
-        <div class="overline mb-4">
-          OVERLINE
-        </div>
-        <v-list-item-title class="headline mb-1">
-          Headline 5
-        </v-list-item-title>
-        <v-list-item-subtitle
-          >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
-        >
+        <v-list-item-title class="headline mb-1">conv2d</v-list-item-title>
+
+        <v-list-item-subtitle>this is conv2d</v-list-item-subtitle>
       </v-list-item-content>
 
       <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
@@ -29,16 +23,21 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      childmsg:'我是子组件的值'
+    };
   },
   methods: {
     remove() {
       //向父组件传递一个空参数，让父组件执行remove方法
       this.$emit("remove");
     },
-    index(){
+    index() {
       this.$emit("count");
-    }
+    },
   },
+  mounted(){
+    this.$emit('getmsg', this.childmsg)
+  }
 };
 </script>
